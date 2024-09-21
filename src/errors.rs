752cbfe9382949e70c7bca96cfe10bae08ae8ec9
Austyn Krutsinger <akrutsinger@gmail.com>
2024-09-20 19:39:41 -1000

    Simplify errors
    In the beginning error_set seemed like a nice crate to use, but as
    errors were added, so were many duplicant error types. Really, with a
    library this simple, `thiserror` is probably the better approach to
    take.

--- src/errors.rs
+++ src/errors.rs
@@ -5,7 +5,7 @@ error_set! {
     ///
     /// This enum encapsulates all possible error types that may arise when using the various
     /// modules of the `mcdm` crate, such as during normalization, weighting, or ranking operations.
-    McdmError = MethodError || NormalizationError || WeightingError;
+    McdmError = MethodError;
 
     /// Error type for method-specific failures.
     ///
@@ -16,56 +16,30 @@ error_set! {
         /// A general error placeholder. This should be replaced with more specific error types as
         /// the library grows.
         AnError,
-    } || NormalizationError;
-
-    /// Error type for normalization-related failures.
-    ///
-    /// This variant captures all the errors that may occur during the normalization of a decision
-    /// matrix. These errors may include issues such as dimension mismatches, incorrect criteria
-    /// types, or invalid ranges.
-    NormalizationError = {
-        /// Thrown when the dimensions of the decision matrix do not match the expected dimensions
-        /// (e.g., criteria types array has a different length than the number of columns in the matrix).
-        DimensionMismatch,
-        /// Thrown when the criteria types array contains invalid values (i.e., anything other than
-        /// -1 for cost or 1 for profit).
-        NormalizationCriteraTypeMismatch,
-        /// Thrown when an empty matrix is provided for normalization.
-        EmptyMatrix,
-        /// Thrown when the minimum value of a criterion cannot be determined (e.g., for certain criteria).
-        NoMinimum,
-        /// Thrown when the maximum value of a criterion cannot be determined.
-        NoMaximum,
-        /// Thrown when all values of a criterion are the same, resulting in a zero range.
-        /// Effectively a division by zero error.
-        ZeroRange,
     } || ValidationError;
-
-    /// Error type for weighting-related failures.
-    ///
-    /// This variant captures errors encountered during the calculation of weights for the decision
-    /// matrix criteria. These errors may involve invalid matrix dimensions, lack of minimum/maximum
-    /// values, or incorrect weight ranges.
-    WeightingError = {
-        /// Thrown when the dimensions of the weight vector or decision matrix do not match.
-        DimensionMismatch,
-        /// Thrown when an empty matrix is provided for weight calculation.
-        EmptyMatrix,
-        /// Thrown when no minimum value for a criterion can be found, making it impossible to
-        /// compute weights.
-        NoMinimum,
-        /// Thrown when no maximum value for a criterion can be found.
-        NoMaximum,
-        /// Thrown when all values of a criterion are equal, leading to undefined weight calculations.
-        ZeroRange,
-    };
 
     /// Error type for validation failures.
     ///
-    /// This variant is used to capture validation-related errors that occur during checks of input
-    /// data, such as invalid values provided by the user.
+    /// This variant of `McdmError` captures validation-related errors that occur during checks of
+    /// input data, such as invalid values provided by the user. This variant is used for weighting
+    /// and normalization functions.
     ValidationError = {
-        /// Thrown when an input value is invalid (e.g., a criterion type that is neither -1 nor 1).
+        /// Dimensions of the weight vector, normalization, or decision matrix do not match.
+        DimensionMismatch,
+        /// Empty matrix is provided for weight calculation.
+        EmptyMatrix,
+        /// Input value is invalid (e.g., a criterion type that is neither -1 nor 1).
         InvalidValue,
+        /// Criteria types array contains invalid values (i.e., anything other than
+        /// -1 for cost or 1 for profit).
+        NormalizationCriteraTypeMismatch,
+        /// No minimum value for a criterion can be found, making it impossible to
+        /// compute weights.
+        NoMinimum,
+        /// No maximum value for a criterion can be found.
+        NoMaximum,
+        /// Value of zero is improperly included in a calculation, leading to possible undefined
+        /// calculations.
+        ZeroRange,
     };
 }
