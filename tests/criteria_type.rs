e49e54667ea43a4686b76c5bc07ae788d2fa352a
Austyn Krutsinger <akrutsinger@gmail.com>
2024-09-30 19:44:04 -1000

    Add tests for CriteriaType enum

---
+++ tests/criteria_type.rs
@@ -0,0 +1,33 @@
+mod criteria_type_tests {
+    use mcdm::errors::ValidationError;
+    use mcdm::CriteriaType;
+
+    #[test]
+    fn test_from_valid_values() {
+        let input = vec![-1, 1, -1, 1];
+        let expected = vec![
+            CriteriaType::Cost,
+            CriteriaType::Profit,
+            CriteriaType::Cost,
+            CriteriaType::Profit,
+        ];
+        let result = CriteriaType::from(input);
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_from_invalid_values() {
+        let input = vec![-1, 2, 1];
+        let result = CriteriaType::from(input);
+        assert!(result.is_err());
+        assert!(matches!(result.unwrap_err(), ValidationError::InvalidValue));
+    }
+
+    #[test]
+    fn test_profits_length() {
+        let len = 5;
+        let result = CriteriaType::profits(len);
+        assert_eq!(result.len(), len);
+        assert!(result.iter().all(|x| *x == CriteriaType::Profit));
+    }
+}
