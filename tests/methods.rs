3505a814bd785b8f451528d209b9d5104ece0d2f
Austyn Krutsinger <akrutsinger@gmail.com>
2024-10-02 21:24:59 -1000

    Fix TOPSIS tests

--- tests/methods.rs
+++ tests/methods.rs
@@ -4,17 +4,20 @@ use ndarray::array;
 
 mod topsis_tests {
     use super::*;
+    use mcdm::normalization::{MinMax, Normalize};
 
     #[test]
     fn test_rank() {
         let matrix = array![
-            [0.1, 0.1, 0.1, 0.1],
-            [0.2, 0.2, 0.2, 0.2],
-            [0.3, 0.3, 0.3, 0.3]
+            [2.9, 2.31, 0.56, 1.89],
+            [1.2, 1.34, 0.21, 2.48],
+            [0.3, 2.48, 1.75, 1.69]
         ];
         let weights = array![0.25, 0.25, 0.25, 0.25];
-        let ranking = TOPSIS::rank(&matrix, &weights).unwrap();
-        assert_abs_diff_eq!(ranking, array![0.0, 0.5, 1.0], epsilon = 1e-5);
+        let criteria_types = mcdm::CriteriaType::from(vec![-1, 1, 1, -1]).unwrap();
+        let normalized_matrix = MinMax::normalize(&matrix, &criteria_types).unwrap();
+        let ranking = TOPSIS::rank(&normalized_matrix, &weights).unwrap();
+        assert_abs_diff_eq!(ranking, array![0.47089549, 0.27016783, 1.0], epsilon = 1e-5);
     }
 
     #[test]
