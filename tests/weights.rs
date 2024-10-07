c7ec1dabbde6bb873e37199d11499cb9bb123260
Austyn Krutsinger <akrutsinger@gmail.com>
2024-10-06 15:44:46 -1000

    Add MEREC weighting method test

--- tests/weights.rs
+++ tests/weights.rs
@@ -38,7 +38,27 @@ mod entropy_tests {
     }
 }
 
-mod merec_tests {}
+mod merec_tests {
+    use super::*;
+    use mcdm::normalization::{Linear, Normalize};
+    use mcdm::CriteriaType;
+
+    #[test]
+    fn test_weight() {
+        let matrix = array![
+            [2.9, 2.31, 0.56, 1.89],
+            [1.2, 1.34, 0.21, 2.48],
+            [0.3, 2.48, 1.75, 1.69]
+        ];
+
+        // NOTE: This is the "oposite" of the criteria types we'd expect to use for other methods.
+        let criteria_types = CriteriaType::from(vec![1, -1, -1, 1]).unwrap();
+        let normalized_matrix = Linear::normalize(&matrix, &criteria_types).unwrap();
+        let weights = Merec::weight(&normalized_matrix).unwrap();
+        let expected_weights = array![0.40559526, 0.14185966, 0.37609753, 0.07644754];
+        assert_abs_diff_eq!(weights, expected_weights, epsilon = 1e-5);
+    }
+}
 
 mod standard_deviation_tests {
     use super::*;
