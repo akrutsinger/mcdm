1d03d3d9837637123bb802c411df1569ef385edc
Austyn Krutsinger <akrutsinger@gmail.com>
2025-01-25 09:41:00 -1000

    Remove unused function `get_ideal_from_bounds`
    This function in DMatrixExt was an oversight left over when refactoring
    the bounds checking into validation.rs.

--- src/dmatrix_ext.rs
+++ src/dmatrix_ext.rs
@@ -1,4 +1,3 @@
-use crate::CriterionType;
 use nalgebra::{DMatrix, DVector};
 
 pub trait DMatrixExt {
@@ -11,16 +10,6 @@ pub trait DMatrixExt {
     /// # Panics
     /// This method panics if the number of columns in the matrix does not match the length of the vector.
     fn scale_columns(&self, vector: &DVector<f64>) -> DMatrix<f64>;
-
-    /// Returns a matrix with the ideal reference values for each criteria.
-    ///
-    /// The resulting matrix is an $n$-by-2 matrix where $n$ is the number of criteria. If the
-    /// criterion is a cost, the ideal will be the minimium value of the associated bounds. If the
-    /// criterion is a profit, the ideal is the maximum value of the associated bounds.
-    ///
-    /// # Arguments
-    /// * `criteria_types` - A vector of `CriteriaTypes` values indicating the type of the criteria.
-    fn get_ideal_from_bounds(&self, criteria_types: &[CriterionType]) -> DMatrix<f64>;
 }
 
 impl DMatrixExt for DMatrix<f64> {
@@ -35,24 +24,5 @@ impl DMatrixExt for DMatrix<f64> {
             result.column_mut(m).scale_mut(weights[m]);
         }
         result
-    }
-
-    fn get_ideal_from_bounds(&self, criteria_types: &[CriterionType]) -> DMatrix<f64> {
-        let mut ideal: DMatrix<f64> = DMatrix::zeros(self.nrows(), self.ncols());
-
-        // Each row is expected to represent [min, max] in an already validated bounds
-        for (i, row) in self.row_iter().enumerate() {
-            match criteria_types[i] {
-                CriterionType::Cost => {
-                    ideal[(i, 0)] = row.min();
-                    ideal[(i, 1)] = row.min();
-                }
-                CriterionType::Profit => {
-                    ideal[(i, 0)] = row.max();
-                    ideal[(i, 1)] = row.max();
-                }
-            }
-        }
-        ideal
     }
 }
